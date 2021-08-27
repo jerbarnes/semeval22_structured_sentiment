bf4afb2dd2a62f50b7ad1670150a73ade8cf689f
jeremy <jbarnesspain@gmail.com>
2021-08-27 16:42:53 +0200

    add data conversion scripts

---
+++ baselines/sequence_labeling/convert_to_bio.py
@@ -0,0 +1,234 @@
+import json
+import os
+import argparse
+from nltk.tokenize.simple import SpaceTokenizer
+
+tk = SpaceTokenizer()
+
+
+def get_bio_target(opinion):
+    try:
+        text, idxs = opinion["Target"]
+    # will throw exception if the opinion target is None type
+    except TypeError:
+        return []
+    except ValueError:
+        return []
+    # get the beginning and ending indices
+    if len(text) > 1:
+        updates = []
+        #
+        for t, idx in zip(text, idxs):
+            bidx, eidx = idx.split(":")
+            bidx = int(bidx)
+            eidx = int(eidx)
+            polarity = opinion["Polarity"]
+            target_tokens = t.split()
+            label = "-targ-{0}".format(polarity)
+            #
+            tags = []
+            for i, token in enumerate(target_tokens):
+                if i == 0:
+                    tags.append("B" + label)
+                else:
+                    tags.append("I" + label)
+            updates.append((bidx, tags))
+        return updates
+    else:
+        bidx, eidx = idxs[0].split(":")
+        bidx = int(bidx)
+        eidx = int(eidx)
+        polarity = opinion["Polarity"]
+        target_tokens = text[0].split()
+        label = "-targ-{0}".format(polarity)
+        #
+        tags = []
+        for i, token in enumerate(target_tokens):
+            if i == 0:
+                tags.append("B" + label)
+            else:
+                tags.append("I" + label)
+        return [(bidx, tags)]
+
+def get_bio_expression(opinion):
+    try:
+        text, idxs = opinion["Polar_expression"]
+    # will throw exception if the opinion target is None type
+    except TypeError:
+        return []
+    except ValueError:
+        return []
+    # get the beginning and ending indices
+    if len(text) > 1:
+        updates = []
+        #
+        for t, idx in zip(text, idxs):
+            bidx, eidx = idx.split(":")
+            bidx = int(bidx)
+            eidx = int(eidx)
+            polarity = opinion["Polarity"]
+            target_tokens = t.split()
+            label = "-exp-{0}".format(polarity)
+            #
+            tags = []
+            for i, token in enumerate(target_tokens):
+                if i == 0:
+                    tags.append("B" + label)
+                else:
+                    tags.append("I" + label)
+            updates.append((bidx, tags))
+        return updates
+    else:
+        bidx, eidx = idxs[0].split(":")
+        bidx = int(bidx)
+        eidx = int(eidx)
+        polarity = opinion["Polarity"]
+        target_tokens = text[0].split()
+        label = "-exp-{0}".format(polarity)
+        #
+        tags = []
+        for i, token in enumerate(target_tokens):
+            if i == 0:
+                tags.append("B" + label)
+            else:
+                tags.append("I" + label)
+        return [(bidx, tags)]
+
+def get_bio_holder(opinion):
+    try:
+        text, idxs = opinion["Source"]
+    # will throw exception if the opinion target is None type
+    except TypeError:
+        return []
+    except ValueError:
+        return []
+    # get the beginning and ending indices
+    if len(text) > 1:
+        updates = []
+        #
+        for t, idx in zip(text, idxs):
+            bidx, eidx = idx.split(":")
+            bidx = int(bidx)
+            eidx = int(eidx)
+            target_tokens = t.split()
+            label = "-holder"
+            #
+            tags = []
+            for i, token in enumerate(target_tokens):
+                if i == 0:
+                    tags.append("B" + label)
+                else:
+                    tags.append("I" + label)
+            updates.append((bidx, tags))
+        return updates
+    else:
+        bidx, eidx = idxs[0].split(":")
+        bidx = int(bidx)
+        eidx = int(eidx)
+        target_tokens = text[0].split()
+        label = "-holder"
+        #
+        tags = []
+        for i, token in enumerate(target_tokens):
+            if i == 0:
+                tags.append("B" + label)
+            else:
+                tags.append("I" + label)
+        return [(bidx, tags)]
+
+
+def replace_with_labels(labels, offsets, bidx, tags):
+    # There are some annotations that missed token level (left out a leading character) that we need to fix
+    try:
+        token_idx = offsets.index(bidx)
+        for i, tag in enumerate(tags):
+            labels[i + token_idx] = tag
+        return labels
+    except:
+        return labels
+
+def restart_orphans(labels):
+    """Wen opinion expression tags are written on top of previous expression tags,
+    I-tags can be orphaned, so they do not correspond with the previous tag. We reset these to a B
+
+        labels : list(Str) tag sequence for a sentence.
+    """
+    prev = "O"
+    for tag_idx,tag in enumerate(labels):
+        if tag[0] == "I":
+            if prev == "O" or (len(prev)>1 and tag[1:] != prev[1:]):
+                labels[tag_idx] = "B"+tag[1:] #Replace I with B since contents is different from prev
+                #print("correcting", prev, tag)
+        prev = labels[tag_idx]
+    return labels
+
+
+def create_bio_labels(text, opinions):
+    offsets = [l[0] for l in tk.span_tokenize(text)]
+    #
+    columns = ["Source", "Target", "Polar_expression"]
+    labels = {c: ["O"] * len(offsets) for c in columns}
+    #
+    anns = {c: [] for c in columns}
+
+
+    # TODO: deal with targets which can have multiple polarities, due to
+    # contrasting polar expressions. At present the last polarity wins.
+    for o in opinions:
+        try:
+            anns["Source"].extend(get_bio_holder(o))
+        except:
+            pass
+        try:
+            anns["Target"].extend(get_bio_target(o))
+        except:
+            pass
+        try:
+            anns["Polar_expression"].extend(get_bio_expression(o))
+        except:
+            pass
+    #
+    for c in columns:
+        for bidx, tags in anns[c]:
+            labels[c] = replace_with_labels(labels[c], offsets, bidx, tags)
+        labels[c] = restart_orphans(labels[c])
+    return labels
+
+
+def to_bio(dataset):
+    bio_dataset = []
+    for i, sent in enumerate(dataset):
+        idx = sent["sent_id"]
+        text = sent["text"]
+        opinions = sent["opinions"]
+        tokens = text.split()
+        labels = create_bio_labels(text, opinions)
+        bio_sent = {"sent_id": idx,
+                    "text": tokens,
+                    "sources": labels["Source"],
+                    "targets": labels["Target"],
+                    "expressions": labels["Polar_expression"]
+                    }
+        bio_dataset.append(bio_sent)
+    return bio_dataset
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--datasets", default=["darmstadt_unis", "multibooked_ca", "multibooked_eu", "norec", "opener_en", "opener_es"])
+
+    args = parser.parse_args()
+
+    for dataset in args.datasets:
+        for data_split in os.listdir(os.path.join("../../data", dataset)):
+            if data_split in ["train.json", "dev.json", "test.json"]:
+                with open(os.path.join("../../data", dataset, data_split)) as o:
+                    split = json.load(o)
+                bio_split = to_bio(split)
+                os.makedirs(os.path.join("data", "extraction", dataset), exist_ok=True)
+                with open(os.path.join("data", "extraction", dataset, data_split), "w") as outfile:
+                    for example in bio_split:
+                        json.dump(example, outfile)
+                        outfile.write("\n")
+
+
---
+++ baselines/sequence_labeling/convert_to_rels.py
@@ -0,0 +1,184 @@
+from nltk.tokenize.simple import SpaceTokenizer
+from convert_to_bio import get_bio_holder, get_bio_target, get_bio_expression, replace_with_labels
+import itertools
+import json
+import os
+import numpy as np
+import argparse
+
+def create_bio_labels(text, opinion):
+    tk = SpaceTokenizer()
+    #
+    offsets = [l[0] for l in tk.span_tokenize(text)]
+    #
+    columns = ["Source", "Target", "Polar_expression"]
+    labels = {c: ["O"] * len(offsets) for c in columns}
+    #
+    anns = {c: [] for c in columns}
+    # TODO: deal with targets which can have multiple polarities, due to
+    # contrasting polar expressions. At present the last polarity wins.
+    try:
+        anns["Source"].extend(get_bio_holder(opinion))
+    except:
+        pass
+    try:
+        anns["Target"].extend(get_bio_target(opinion))
+    except:
+        pass
+    try:
+        anns["Polar_expression"].extend(get_bio_expression(opinion))
+    except:
+        pass
+    #
+    for c in columns:
+        for bidx, tags in anns[c]:
+            labels[c] = replace_with_labels(labels[c], offsets, bidx, tags)
+    return labels
+
+def convert_to_train_example(sent_id, text, opinions):
+    examples = []
+    if len(opinions) == 0:
+        return []
+    elif len(opinions) > 1:
+        olabels = []
+        ols = []
+        for opinion in opinions:
+            labels = create_bio_labels(text, opinion)
+            ls = {}
+            for c in ["Source", "Target", "Polar_expression"]:
+                labels[c] = [0 if l == "O" else 1 for l in labels[c]]
+                ls[c] = len(set(labels[c]))
+            olabels.append(labels)
+            ols.append(ls)
+            if ls["Target"] > 1:
+                examples.append((sent_id, text, labels["Target"], labels["Polar_expression"], 1))
+            if ls["Source"] > 1:
+                examples.append((sent_id, text, labels["Source"], labels["Polar_expression"], 1))
+        # Iterate over possible combinations
+        x = range(len(olabels))
+        for idx1, idx2 in itertools.combinations(x, 2):
+            if ols[idx1]["Target"] > 1:
+                if olabels[idx1]["Target"] != olabels[idx2]["Target"]:
+                    examples.append((sent_id, text, olabels[idx1]["Target"], olabels[idx2]["Polar_expression"], 0))
+            if ols[idx1]["Source"] > 1:
+                if olabels[idx1]["Source"] != olabels[idx2]["Source"]:
+                    examples.append((sent_id, text, olabels[idx1]["Source"], olabels[idx2]["Polar_expression"], 0))
+            if ols[idx2]["Target"] > 1:
+                if olabels[idx1]["Target"] != olabels[idx2]["Target"]:
+                    examples.append((sent_id, text, olabels[idx2]["Target"], olabels[idx1]["Polar_expression"], 0))
+            if ols[idx2]["Source"] > 1:
+                if olabels[idx1]["Source"] != olabels[idx2]["Source"]:
+                    examples.append((sent_id, text, olabels[idx2]["Source"], olabels[idx1]["Polar_expression"], 0))
+    else:
+        labels = create_bio_labels(text, opinions[0])
+        # convert to boolean and
+        ls = {}
+        for c in ["Source", "Target", "Polar_expression"]:
+            labels[c] = [0 if l == "O" else 1 for l in labels[c]]
+            ls[c] = len(set(labels[c]))
+        # check if more than one set of labels and if so, add to examples
+        if ls["Target"] > 1:
+            examples.append((sent_id, text, labels["Target"], labels["Polar_expression"], 1))
+        if ls["Source"] > 1:
+            examples.append((sent_id, text, labels["Source"], labels["Polar_expression"], 1))
+    return examples
+
+def create_relation_jsons(filename, outfile):
+    data = []
+    with open(filename) as infile:
+        for sent in json.load(infile):
+            data.extend(convert_to_train_example(sent["sent_id"], sent["text"], sent["opinions"]))
+    json_data = []
+    for sent_id, text, e1, e2, label in data:
+        json_data.append({"sent_id": sent_id, "text": text, "e1": e1, "e2": e2, "label": label})
+    with open(outfile, "w") as o:
+        for d in json_data:
+            json.dump(d, o)
+            o.write("\n")
+
+
+def break_up_predictions(pred):
+    # when there is more than one span in a prediction,
+    # convert to several instances with a single span each
+    #
+    # No spans in prediction
+    if pred.sum() == 0:
+        return [pred.tolist()]
+    # if everything is a span
+    if pred.all() > 0:
+        return [[1] * len(pred)]
+    # If there are spans in prediction
+    idxs = []
+    bidx = None
+    for i, p in enumerate(pred):
+        if p > 0 and bidx is None:
+            bidx = i
+        if p == 0 and bidx is not None:
+            idxs.append((bidx, i))
+            bidx = None
+        if i == len(pred) - 1 and bidx is not None:
+            idxs.append((bidx, i+1))
+    # convert each bidx, eidx to a np array of the size of pred
+    # with 1 for each span
+    preds = []
+    for bidx, eidx in idxs:
+        l = [0] * len(pred)
+        for i in range(bidx, eidx):
+            l[i] = 1
+        preds.append(l)
+    return preds
+
+def break_up_expressions(pred, label2idx):
+    # when there is more than one span in a prediction,
+    # convert to several instances with a single span each
+    #
+    # No spans in prediction
+    if pred.sum() == 0:
+        return [pred.tolist()], []
+    # if everything is a span
+    if pred.all() > 0:
+        full_label = label2idx.idx2label["expressions"][int(pred[0])]
+        polarity = full_label.split("-")[-1]
+        return [[1] * len(pred)], [polarity]
+    # If there are spans in prediction
+    idxs = []
+    bidx = None
+    polarity = None
+    for i, p in enumerate(pred):
+        if p > 0 and bidx is None:
+            bidx = i
+            full_label = label2idx.idx2label["expressions"][int(p)]
+            polarity = full_label.split("-")[-1]
+        if p == 0 and bidx is not None:
+            idxs.append((bidx, i, polarity))
+            bidx = None
+            polarity = None
+        if i == len(pred) - 1 and bidx is not None:
+            idxs.append((bidx, i+1, polarity))
+    # convert each bidx, eidx to a np array of the size of pred
+    # with 1 for each span
+    preds = []
+    polarities = []
+    for bidx, eidx, polarity in idxs:
+        l = [0] * len(pred)
+        for i in range(bidx, eidx):
+            l[i] = 1
+        preds.append(l)
+        polarities.append(polarity)
+    return preds, polarities
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--datasets", default=["darmstadt_unis", "multibooked_ca", "multibooked_eu", "norec", "opener_en", "opener_es"])
+
+    args = parser.parse_args()
+
+    for dataset in args.datasets:
+        for data_split in os.listdir(os.path.join("../../data", dataset)):
+            if data_split in ["train.json", "dev.json", "test.json"]:
+                infile = os.path.join("../../data", dataset, data_split)
+                os.makedirs(os.path.join("data", "relations", dataset),
+                            exist_ok=True)
+                outfile = os.path.join("data", "relations", dataset, data_split)
+                create_relation_jsons(infile, outfile)
