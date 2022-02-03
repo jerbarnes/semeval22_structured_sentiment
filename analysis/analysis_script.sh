063b9bacd67a97f85acddf0ac0bff3ca948ad8fe
jeremy <jbarnesspain@gmail.com>
2022-02-03 23:29:50 +0100

    updated script

--- analysis/analysis_script.sh
+++ analysis/analysis_script.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# location of the predictions.json file
+PREDFILE=$1
+
 # domain analysis
-python3 domain_analysis.py ../data/norec/test_labeled.json predictions.json metadata.json
+python3 domain_analysis.py ../data/norec/test_labeled.json $PREDFILE metadata.json
 
 # negation analysis
-python3 neg_scope_analysis.py ../data/norec/test_labeled.json ../data/norec/test.json negation_test.json 
+python3 neg_scope_analysis.py ../data/norec/test_labeled.json $PREDFILE negation_test.json
